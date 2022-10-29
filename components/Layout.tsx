847100c4cae14f10244692dd5f458cb08bee2e05
aidilrx04 <mrcrayv14@gmail.com>
2022-10-29 22:30:31 +0800

    update meta

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,20 +8,39 @@ function Layout({ children }) {
     <>
       <Head>
         <title>Permainan Teka Kata Ladle</title>
-        <meta property="og:title" content="Permainan Teka Kata Ladle" />
-        <meta property="og:site_name" content="Ladle" />
+        <meta
+          name="description"
+          content="Ladle? Senduk? Cubalah permainan ini sekarang untuk meningkatkan kosa kata dalam Bahasa Melayu"
+        />
+
+        {/* <!-- Facebook Meta Tags --> */}
         <meta property="og:url" content="https://ladle-next.vercel.app" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Permainan Teka Kata Ladle" />
         <meta
           property="og:description"
-          content="Ladle? Senduk?
-
-Cubalah permainan ini sekarang untuk meningkatkan kosa kata dalam Bahasa Melayu"
+          content="Ladle? Senduk? Cubalah permainan ini sekarang untuk meningkatkan kosa kata dalam Bahasa Melayu"
         />
-        <meta property="og:type" content="website" />
         <meta
           property="og:image"
           content="https://mobileimages.lowes.com/productimages/4e36c363-8d98-4689-b41d-640597b2d2ac/08712499.jpg?size=pdhism"
         />
+
+        {/* <!-- Twitter Meta Tags --> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="ladle-next.vercel.app" />
+        <meta property="twitter:url" content="https://ladle-next.vercel.app" />
+        <meta name="twitter:title" content="Permainan Teka Kata Ladle" />
+        <meta
+          name="twitter:description"
+          content="Ladle? Senduk? Cubalah permainan ini sekarang untuk meningkatkan kosa kata dalam Bahasa Melayu"
+        />
+        <meta
+          name="twitter:image"
+          content="https://mobileimages.lowes.com/productimages/4e36c363-8d98-4689-b41d-640597b2d2ac/08712499.jpg?size=pdhism"
+        />
+
+        {/* <!-- Meta Tags Generated via https://www.opengraph.xyz --> */}
       </Head>
       <Navigation />
       <Viewidth>
